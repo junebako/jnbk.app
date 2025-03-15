758411aa1df4387f7ef7aa5ebd3e4521785754c3
juneboku <juneboku@gmail.com>
2025-03-15 15:11:20 +0900

    インデントが変だった…

--- app/controllers/nikki_controller.rb
+++ app/controllers/nikki_controller.rb
@@ -1,14 +1,14 @@
 class NikkiController < ApplicationController
-    rescue_from Nikki::NotFound, with: :not_found
+  rescue_from Nikki::NotFound, with: :not_found
 
-    def show
-      date = params[:date]
-      nikki = Nikki.new("juneboku").find_by(date:)
+  def show
+    date = params[:date]
+    nikki = Nikki.new("juneboku").find_by(date:)
 
-      redirect_to(nikki.url, allow_other_host: true)
-    end
-
-    def not_found
-      render(plain: "Nikki Not Found!", status: :not_found)
-    end
+    redirect_to(nikki.url, allow_other_host: true)
   end
+
+  def not_found
+    render(plain: "Nikki Not Found!", status: :not_found)
+  end
+end
